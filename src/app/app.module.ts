cdfe58bb0fc5e19d8558bf6c962c5fae78649ce6
Dru@LAPTOP-5JDRQNMO <drufortinera@gmail.com>
2022-03-21 22:48:35 -0300

    ya estan todas las secciones html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { PersonaComponent } from './componentes/persona/persona.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { IdiomasComponent } from './componentes/idiomas/idiomas.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PersonaComponent,
+    EstudiosComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    IdiomasComponent,
+    ProyectosComponent
   ],
   imports: [
     BrowserModule
